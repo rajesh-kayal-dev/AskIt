c4fe58745d75127c6b88a4f165455ec8a58c796e
rajesh-kayal-dev <rk6289943975@gmail.com>
2026-07-11 02:52:21 +0530

    feat: initialize frontend project with React, Redux Toolkit, and user authentication state management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
 import { useEffect } from 'react';
 import { AppRoutes } from './routes/AppRoutes';
 import getCurrentUser from './features/getCurrentUser';
+import { useDispatch } from 'react-redux';
+import { setUserData } from './redux/userSlice';
+import type { AppDispatch } from './redux/store';
 
 const App = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
   useEffect(() => {
     const getUser = async () => {
-      await getCurrentUser();
+      const response = await getCurrentUser();
+      if (response) {
+        dispatch(setUserData(response));
+      }
     };
     getUser();
-  }, []);
+  }, [dispatch]);
 
   return <AppRoutes />;
 };
