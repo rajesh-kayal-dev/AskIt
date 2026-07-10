4c34f19a25fcf0fd6bdcfcaf19bd9b1eff1630d9
rajesh-kayal-dev <rk6289943975@gmail.com>
2026-07-11 02:52:59 +0530

    feat: initialize frontend application with React, Redux, Vite, and dashboard/login pages

--- src/features/getCurrentUser.ts
+++ src/features/getCurrentUser.ts
@@ -1,10 +1,11 @@
 import { api } from './auth/services/api';
+import type { User } from './auth/types/auth.types';
 
-const getCurrentUser = async () => {
+const getCurrentUser = async (): Promise<User | undefined> => {
   try {
-    const { data } = await api.get('/user/me');
-    console.log(data);
-    return data;
+    const response = await api.get<any, User>('/user/me');
+    console.log("Full response:", response);
+    return response;
   } catch (error) {
     console.log(error);
   }
